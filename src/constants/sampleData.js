81935676d74e76c9f9abcbd85bf2f58a9bf1e185
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-07-14 14:39:23 +0545

    dashboard ui added

--- src/constants/sampleData.js
+++ src/constants/sampleData.js
@@ -212,3 +212,99 @@ export const sampleMessage = [
     createdAt: "2024-06-12T10:41:20.630Z",
   },
 ];
+
+export const dashboardData = {
+  users: [
+    {
+      name: "John Doe",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "1",
+      username: "john_do4",
+      friends: 20,
+      groups: 5,
+    },
+    {
+      name: "John show",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "2",
+      username: "john_so4",
+      friends: 50,
+      groups: 2,
+    },
+  ],
+
+  chats: [
+    {
+      name: "LabadBass Group",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "1",
+      groupChat: false,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John doe",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+    {
+      name: "lau*a Group",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "2",
+      groupChat: false,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John Boe",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+  ],
+  messages: [
+    {
+      attachments: [
+        {
+          public_id: "asda 2",
+          url: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      ],
+
+      content: "L*uda ka Message hai",
+      _id: "sadsaa",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Chaman",
+      },
+
+      chat: "chatId",
+      groupChat: false,
+      createdAt: "2024-07-17-12T10:41:30.630Z",
+    },
+    {
+      attachments: [
+        {
+          public_id: "asda 3",
+          url: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      ],
+
+      content: "L*uda ka Message hai",
+      _id: "sadsa",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Chameli",
+      },
+
+      chat: "chatId",
+      groupChat: true,
+      createdAt: "2024-07-17-12T10:41:30.630Z",
+    },
+  ],
+};
--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { Avatar, Stack } from "@mui/material";
+import { dashboardData } from "../../constants/sampleData";
+import { transformImage } from "../../lib/features";
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar} />
+    ),
+  },
+  {
+    field: "content",
+    headerName: "Content",
+    headerClassName: "table-header",
+    width: 400,
+  },
+  {
+    field: "sender",
+    headerName: "Sent By",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Stack>
+        <Avatar alt={params.row.sender.name} src={params.row.sender.avatar} />
+        <span>{params.row.sender.name}</span>
+      </Stack>
+    ),
+  },
+  {
+    field: "chat",
+    headerName: "Chat",
+    headerClassName: "table-header",
+    width: 220,
+  },
+  {
+    field: "groupChat",
+    headerName: "Group Chat",
+    headerClassName: "table-header",
+    width: 100,
+  },
+  {
+    field: "createdAt",
+    headerName: "Time",
+    headerClassName: "table-header",
+    width: 250,
+  },
+];
 
 function UserManagement() {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {}, []);
   return (
     <AdminLayout>
-      <div>UserManagement</div>
+      <Table heading={"All Users"} columns={columns} rows={rows} />
     </AdminLayout>
   );
 }
